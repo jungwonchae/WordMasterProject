4eb70e4e8033eee6c5e76b3cb55efae298a6055f
jungwonchae <jjungwon0222@gmail.com>
2022-09-07 15:35:49 +0900

    modified WordCRUD class

--- src/main/java/org/example/WordManager.java
+++ src/main/java/org/example/WordManager.java
@@ -32,7 +32,7 @@ public class WordManager {
                 wordCRUD.addWord();
             }
             else if(menu == 1) {
-                //list
+                wordCRUD.listAll();
             }
         }
     }
