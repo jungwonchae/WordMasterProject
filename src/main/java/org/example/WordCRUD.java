fa36b55448c64361577a7f19d50881f217c090a4
jungwonchae <jjungwon0222@gmail.com>
2022-09-17 15:41:21 +0900

    updated save file function

--- src/main/java/org/example/WordCRUD.java
+++ src/main/java/org/example/WordCRUD.java
@@ -1,9 +1,6 @@
 package org.example;
 
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -127,6 +124,19 @@ public class WordCRUD implements ICRUD{
             br.close();
             System.out.println("==> " + cnt + "개 로딩 완료!!");
 
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void saveFile() {
+        try {
+            PrintWriter pr = new PrintWriter(new FileWriter(fname));
+            for(Word one : list) {
+                pr.write(one.toFileString() + "\n");
+            }
+            pr.close();
+            System.out.println("==> 데이터 저장 완료 !!!");
         } catch (IOException e) {
             e.printStackTrace();
         }
